2526a8bad58685cb6478febdc02df5624336f521
unknown <hamza_amir@yahoo.com>
2021-01-31 20:15:46 +0600

    Adding filtering functionality

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,9 +34,9 @@ namespace API.Controllers
 
         // GET: api/Products      
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
